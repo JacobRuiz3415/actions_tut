2d4fc28b3cd6974dfd599d06f04152beb225cd30
Jacob Ruiz <112045551+JacobRuiz3415@users.noreply.github.com>
2026-09-05 21:14:45 +0000

    added some things

---
+++ feed.py
@@ -0,0 +1,11 @@
+import yaml
+import xmletree.ElementTree as xml_tree
+ 
+with open('feed.yml', 'r') as file:
+    yaml_data = yaml.safe_load(file)
+
+    rss_element - xml_tree.element('rss'{
+        
+
+
+    })
